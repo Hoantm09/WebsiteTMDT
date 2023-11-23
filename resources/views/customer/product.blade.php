f25de0cac8dc426eac3316a1e9bb80e2b7978514
congquyenpro <quyen.nc207699@sis.hust.edu.vn>
2023-11-23 11:48:07 +0700

    using template

--- resources/views/customer/product.blade.php
+++ resources/views/customer/product.blade.php
@@ -170,7 +170,22 @@
                                     </div>
                                 </div>
                                 <div class="tab-pane fade" id="contact-justified" role="tabpanel" aria-labelledby="contact-tab-justified">
-                                    ...
+                                    <p style="margin-bottom:10px !important"><span style="font-size: 14pt;"><b>Cách sử dụng nước hoa:</b></span></p>
+        <ul>
+    <li style="font-weight: 400"><span style="font-weight: 400">Xịt nước hoa khi cơ thể sạch, khô, hoặc sau khi thoa dưỡng ẩm để giữ mùi lâu hơn</span></li>
+    <li style="font-weight: 400"><span style="font-weight: 400">Giữ chai xịt cách cơ thể khoảng 12cm – 15cm và hướng đầu vòi xịt về mình. Nếu nước hoa làm ướt da thì nghĩa là bạn đang xịt ở khoảng cách quá gần. Chờ "điểm mạch" khô tự nhiên mà không chà xát(thói quen này làm bay mùi và biến mùi nước hoa)</span></li>
+    <li style="font-weight: 400"><span style="font-weight: 400">Xịt nước hoa vào các điểm mạch đập(cổ, ngực, các điểm mạch, cẳng tay hoặc khuỷu tay): đây là những vùng có mạch máu nằm gần bề mặt da. Các điểm này ấm hơn những nơi khác, hơi ấm giúp khuếch tán mùi hương tốt.</span></li>
+    <li style="font-weight: 400"><span style="font-weight: 400">Sử dụng nước hoa phù hợp theo mùa, thời tiết: vì sức nóng và độ ẩm sẽ tăng mạnh mùi hương, nên hãy sử dụng một loại nước hoa có mùi dịu nhẹ hơn (các loại được gọi là lotion, cologne hay toilette) vào mùa hè. Hãy để dành các loại nước hoa có tên perfume hay parfum, với nồng độ mùi cao hơn, cho mùa đông.</span></li>
+    <li style="font-weight: 400"><span style="font-weight: 400">Nước hoa có thể bám và tỏa mùi tốt hay không còn phụ thuộc vào cơ địa, thời gian, không gian sử dụng.</span></li>
+    <li style="font-weight: 400"><span style="font-weight: 400">Chọn hương nước hoa phù hợp: nếu bạn chỉ đi dạo phố, đi làm hay đi biển, hãy dùng hương nước hoa ban ngày. Nếu chuẩn bị cho buổi hẹn hò hay ra ngoài ăn tối, có thể bạn nên thử dùng hương nước hoa ban đêm.</span></li>
+    <li style="font-weight: 400"><span style="font-weight: 400">Nước hoa ban đêm thường được xịt lên cổ hoặc gần vùng cổ. Lý do là vì hương nước hoa ban đêm không lưu lại lâu</span></li>
+    <li style="font-weight: 400"><span style="font-weight: 400">Nước hoa ban ngày thường được xịt vào hông hoặc đầu gối. Đó là vì hương nước hoa ban ngày tỏa hương suốt ngày và thơm dai hơn. Bạn nên dùng thêm chút kem dưỡng ẩm gần chỗ định xức nước hoa để mùi hương lưu lại lâu hơn.</span></li>
+    </ul>
+        <p style="margin-top:10px !important"><span style="font-size: 14pt"><b>Bảo quản nước hoa:</b></span></p>
+        <p><span style="font-weight: 400">Nước hoa không chỉ loãng và mất hương thơm theo thời gian, mà còn bị biến màu, biến chất dẫn đến mùi nước hoa có mùi khó chịu. Nếu bảo quản không đúng cách, nước hoa có thể bắt đầu hỏng sau vài tháng.&nbsp;</span></p>
+        <p><b>Ánh sáng: </b><span style="font-weight: 400">tiếp xúc trực tiếp với ánh sáng trong một khoảng thời gian chắc chắn sẽ khiến nước hoa bị biến chất. Nên để nước hoa trong hộp, nơi tối, khô thoáng(tủ đồ, kệ tủ).</span></p>
+        <p><b>Nhiệt độ: </b><span style="font-weight: 400">nhiệt độ dao động quá cao sẽ nhanh chóng làm hỏng mùi hương. Vì vậy để nước hoa trong nhà tắm có khả năng hư hỏng nhanh hơn nhiều so với nước hoa được lưu trữ trong không gian khác(tủ đồ, kệ tủ,...).</span></p>
+        <p style="text-align: center"><iframe src="https://www.youtube.com/embed/tFIR2TCULzw" width="600" height="350" allowfullscreen="allowfullscreen"></iframe></p>
                                 </div>
                             </div>
                         </div>
