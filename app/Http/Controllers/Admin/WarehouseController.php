aa36c68a3d6b663b4b9132266577656de3c87ca0
congquyenpro <quyen.nc207699@sis.hust.edu.vn>
2024-01-19 15:14:09 +0700

    update warehouse

--- app/Http/Controllers/Admin/WarehouseController.php
+++ app/Http/Controllers/Admin/WarehouseController.php
@@ -14,7 +14,10 @@ use App\Models\WarehouseHistoryDetail;
 
 // use App\Repositories\Manager\OrderRepository;
 // use App\Models\Product\FullOrder;
-// use App\Models\Product\OrderDetail; 
+// use App\Models\Product\OrderDetail;
+
+use App\Repositories\Manager\ProductRepository;
+use App\Models\Product;
 
 use Carbon\Carbon;
 use Session;
@@ -30,10 +33,14 @@ class WarehouseController extends Controller
     protected $order;
     protected $order_detail;
 
-    public function __construct(Warehouse $warehouse, WarehouseHistory $warehouse_history, WarehouseHistoryDetail $warehouse_history_detail){
+    protected $product;
+
+    public function __construct(Warehouse $warehouse, WarehouseHistory $warehouse_history, WarehouseHistoryDetail $warehouse_history_detail, Product $product){
         $this->warehouse                    = new WarehouseRepository($warehouse);
         $this->warehouse_history            = new WarehouseRepository($warehouse_history);
         $this->warehouse_history_detail     = new WarehouseRepository($warehouse_history_detail);
+
+        $this->product                      = new ProductRepository($product);
         // $this->order            = new OrderRepository($order);
         // $this->order_detail     = new OrderRepository($order_detail); 
     } 
@@ -94,13 +101,40 @@ class WarehouseController extends Controller
             
                 $warehouse_create = [
                     "product_id"    => $data_item->item,
+                    "size_id"        => $data_item->size_id,
                     "quantity"      => $data_item->quantity,
                     "reserve"       => 0,
                     "expiry_date"   => $data_item->expiry_date,
                 ];
                 $this->warehouse->create($warehouse_create);
+
+                //Tăng số lượng ở Sản phầm khi Nhập kho
+                $update = $this->update_size_number($data_item->item,$data_item->size_id,$data_item->quantity);
+
+
         }
-        return $request;
+        //return $request;
+        return $update;
     }
+
+    //Cập nhật số lượng sản phẩm theo dung tích
+    //Lấy dữ liệu cũ => update
+    public function update_size_number($productId,$sizeId,$new_num){
+        $data = $this->product->getNumBySize($productId,$sizeId);
+        $list_size = json_decode($data[0]->metadata)->data;
+
+        //id của size -1 = index trong mảng
+        $size1 = $list_size[0]->quantity;
+        //$new_num = 1000;
+
+        //Get old number
+        $size_index = $sizeId - 1;
+        $old_numer =  $list_size[$size_index]->quantity;
+        $new_number = $old_numer + $new_num;
+
+        $update_num = $this->product->updateNumBySize($productId,$sizeId,$new_number);
+        return $this->product->send_response(200, $update_num, null);
+    }
+    
     
 }
